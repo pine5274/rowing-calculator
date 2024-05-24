123ddb7e5994215e83908ab9b2a3ef21d7c13146
sou-komatsu <sou-komastu@mahoujin.co.jp>
2024-05-24 11:02:58 +0900

    add coxed

--- src/component/pages/ErgoScoreBoatSpeed.jsx
+++ src/component/pages/ErgoScoreBoatSpeed.jsx
@@ -2,29 +2,21 @@ import React from 'react';
 import Box from '@mui/material/Box'
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
-import ergoScoreBoatSpeed from '../../data/ergoScoreBoatSpeed';
-
-import PropTypes from 'prop-types';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import Link from '@mui/material/Link';
-import Divider from '@mui/material/Divider';
-import TextField from '@mui/material/TextField';
-import InputAdornment from '@mui/material/InputAdornment';
-import Button from '@mui/material/Button';
-import LibraryAddIcon from '@mui/icons-material/LibraryAdd';
+import ergoScoreBoatSpeed2 from '../../data/ergoScoreBoatSpeed';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
-import { styled } from '@mui/material/styles';
-
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
+import { getTheme } from '../../data/theme';
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme(getTheme());
 
 const ErgoScoreBoatSpeed = () => {
 
@@ -52,14 +44,74 @@ const ErgoScoreBoatSpeed = () => {
                         <MenuItem value={'1x'}>1x</MenuItem>
                         <MenuItem value={'2x'}>2x</MenuItem>
                         <MenuItem value={'4x'}>4x</MenuItem>
+                        <MenuItem value={'4x+'}>4x+</MenuItem>
                         <MenuItem value={'2-'}>2-</MenuItem>
                         <MenuItem value={'4-'}>4-</MenuItem>
+                        <MenuItem value={'4+'}>4+</MenuItem>
                         <MenuItem value={'8+'}>8+</MenuItem>
                     </Select>
                 </FormControl>
-                <CorrelationTable boatType={boatType}/>
+                <CorrelationTable2 boatType={boatType}/>
             </Box>
         </Box>
+    );
+}
+
+const CorrelationTable2 = ({ boatType }) => {
+
+    const correlation = ergoScoreBoatSpeed2()[boatType];
+    const head = [
+        'ergo',
+        '50kg',
+        '55kg',
+        '60kg',
+        '65kg',
+        '70kg',
+        '75kg',
+        '80kg',
+        '85kg',
+        '90kg',
+        '95kg',
+        '100kg',
+        '105kg',
+        '110kg',
+    ];
+    const body = correlation.map((value) => {
+        return Object.values(value);
+    });
+
+    return (
+        <TableContainer component={Paper} sx={{my: 2, maxWidth: 950, maxHeight: 600 }}>
+            <Table stickyHeader sx={{ minWidth: 350 }} size="small" aria-label="sculling chose table">
+                <TableHead>
+                    <TableRow >
+                        {head.map((row, i) => 
+                            <TableCell key={i} align="right" sx={{ backgroundColor: theme.palette.background.paper }}>{row}</TableCell>
+                        )}
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {body.map((row, i) => {
+                        return (
+                            <TableRow
+                                key={i}
+                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                            >
+                                {row.map((value, j) => {
+                                    if (j === 0) {
+                                        return <TableCell key={j} align="right" sx={{ backgroundColor: theme.palette.background.paper }}>
+                                                {value.slice(1, 5)}
+                                            </TableCell>
+                                    }
+                                    return <TableCell key={j} align="right">{value.slice(1, 5)}</TableCell>
+                                }
+                                )}
+                            </TableRow>
+                        )}
+                    )}
+                </TableBody>
+            </Table>
+        </TableContainer>
     );
 }
 
